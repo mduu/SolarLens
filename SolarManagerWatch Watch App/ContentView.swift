f1ab5788d531d3a159825af48ce258dc5c590cb4
Marc Dürst <marc@marcduerst.com>
2024-09-28 16:03:33 +0200

    Initial Commit

---
+++ SolarManagerWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SolarManagerWatch Watch App
+//
+//  Created by Marc Dürst on 28.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SolarManagerWatch Watch App/SolarManagerWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarManagerWatchApp.swift
+//  SolarManagerWatch Watch App
+//
+//  Created by Marc Dürst on 28.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct SolarManagerWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
