769003387aec1e37e9aed7735c5d63fbbd8abcab
Marc Dürst <marc@marcduerst.com>
2024-10-05 06:54:24 +0200

    Add solar indicator to overview

--- SolarManagerWatch Watch App/OverviewView.swift
+++ SolarManagerWatch Watch App/OverviewView.swift
@@ -5,19 +5,34 @@
 //  Created by Marc Dürst on 29.09.2024.
 //
 
-
 import SwiftUI
 
 struct OverviewView: View {
     @EnvironmentObject var model: BuildingStateViewModel
-    
+
+    @State private var solarProductionMinValue = 0.0
+    @State private var solarProductionMaxValue = 11.0
+
     var body: some View {
         VStack {
+            HStack {
+                Gauge(
+                    value: model.overviewData.currentSolarProduction,
+                    in: solarProductionMinValue...solarProductionMaxValue
+                ) {
+                    Text("Solar")
+                } currentValueLabel: {
+                    Text(String(format: "%.1f", model.overviewData.currentSolarProduction))
+                }
+                .gaugeStyle(.circular)
+            }
             Image(systemName: "house")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("House Overview")
-            Text("Solar Power: \(model.overviewData.currentSolarProduction, specifier:"%.2f")")
+            Text(
+                "Solar Power: \(model.overviewData.currentSolarProduction, specifier:"%.2f")"
+            )
         }
         .padding()
     }
@@ -25,4 +40,5 @@ struct OverviewView: View {
 
 #Preview {
     OverviewView()
+        .environmentObject(BuildingStateViewModel())
 }
