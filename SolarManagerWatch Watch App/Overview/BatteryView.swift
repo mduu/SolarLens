a0b3160a72e300f98469b006178e77a1e9c7c920
Marc Dürst <marc@marcduerst.com>
2024-10-05 11:45:39 +0200

    Add color to BatteryView

--- SolarManagerWatch Watch App/Overview/BatteryView.swift
+++ SolarManagerWatch Watch App/Overview/BatteryView.swift
@@ -5,7 +5,6 @@
 //  Created by Marc Dürst on 05.10.2024.
 //
 
-
 import SwiftUI
 
 struct BatteryView: View {
@@ -18,17 +17,22 @@ struct BatteryView: View {
                 value: currentBatteryLevel,
                 in: 0...100
             ) {
-                Text("Bat")
             } currentValueLabel: {
                 Text(
                     String(
                         format: "%.0f%%",
                         currentBatteryLevel)
                 )
+                .foregroundStyle(getColor())
             }
-            .gaugeStyle(.accessoryCircularCapacity)
-            
+            .gaugeStyle(.accessoryCircular)
+            .tint(Gradient(colors: [.red, .green, .green, .green]))
+
             Image(systemName: "battery.100percent")
         }
+    }
+
+    private func getColor() -> Color {
+        currentBatteryLevel >= 10 ? .primary : .red
     }
 }
