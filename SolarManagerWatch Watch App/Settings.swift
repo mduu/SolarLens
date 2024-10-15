93fbed0b980ac04cedf18de8d072b5863602f48a
Marc Dürst <marc@marcduerst.com>
2024-10-15 06:06:06 +0200

    Add version info to settings

--- SolarManagerWatch Watch App/Settings.swift
+++ SolarManagerWatch Watch App/Settings.swift
@@ -9,19 +9,34 @@ import SwiftUI
 
 struct Settings: View {
     @EnvironmentObject var model: BuildingStateViewModel
-    
+
     var body: some View {
-        VStack(alignment: .leading)
-        {
+        VStack(alignment: .center) {
             Text("Settings")
                 .font(.largeTitle)
-                    .foregroundColor(.blue)
+                .foregroundColor(.blue)
+                .padding(.bottom, 16)
 
-            Button("Log out")
-            {
+            Button("Log out") {
                 model.logout()
             }
+            
+            Spacer()
+
+            HStack {
+                Text(
+                    "Version: \(Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "")"
+                )
+                Text(
+                    "Build: \(Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "")"
+                )
+            }
+            .frame(maxWidth: .infinity, alignment: .center)
+            .font(.footnote)
+            .foregroundColor(.gray)
+            .padding(.top, 20)
         }
+
     }
 }
 
