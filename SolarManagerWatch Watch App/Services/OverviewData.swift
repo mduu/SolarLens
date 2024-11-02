a613659a047747e0ba97f4ce503c805ad2b6d2b7
Marc Dürst <marc@marcduerst.com>
2024-11-02 16:35:03 +0100

    Fix view model property change propagation

--- SolarManagerWatch Watch App/Services/OverviewData.swift
+++ SolarManagerWatch Watch App/Services/OverviewData.swift
@@ -10,36 +10,37 @@ import Foundation
 class OverviewData: ObservableObject {
     private let minGridConsumptionTreashold: Int = 100
     private let minGridIngestionTreashold: Int = 100
-    
-    var currentSolarProduction: Int = 0
-    var currentOverallConsumption: Int = 0
-    var currentBatteryLevel: Int? = 0
-    var currentBatteryChargeRate: Int? = 0
-    var currentSolarToGrid: Int = 0
-    var currentGridToHouse: Int = 0
-    var currentSolarToHouse: Int = 0
-    var solarProductionMax: Double = 0
-    var hasConnectionError: Bool = false
-    var lastUpdated: Date? = nil
-    var isAnyCarCharing: Bool = false
-    var chargingStations: [ChargingStation] = []
-    
+
+    @Published var currentSolarProduction: Int = 0
+    @Published var currentOverallConsumption: Int = 0
+    @Published var currentBatteryLevel: Int? = 0
+    @Published var currentBatteryChargeRate: Int? = 0
+    @Published var currentSolarToGrid: Int = 0
+    @Published var currentGridToHouse: Int = 0
+    @Published var currentSolarToHouse: Int = 0
+    @Published var solarProductionMax: Double = 0
+    @Published var hasConnectionError: Bool = false
+    @Published var lastUpdated: Date? = nil
+    @Published var isAnyCarCharing: Bool = false
+    @Published var chargingStations: [ChargingStation] = []
+
     init() {
     }
-    
-    init(currentSolarProduction: Int,
-         currentOverallConsumption: Int,
-         currentBatteryLevel: Int?,
-         currentBatteryChargeRate: Int?,
-         currentSolarToGrid: Int,
-         currentGridToHouse: Int,
-         currentSolarToHouse: Int,
-         solarProductionMax: Double,
-         hasConnectionError: Bool,
-         lastUpdated: Date?,
-         isAnyCarCharing: Bool,
-         chargingStations: [ChargingStation])
-    {
+
+    init(
+        currentSolarProduction: Int,
+        currentOverallConsumption: Int,
+        currentBatteryLevel: Int?,
+        currentBatteryChargeRate: Int?,
+        currentSolarToGrid: Int,
+        currentGridToHouse: Int,
+        currentSolarToHouse: Int,
+        solarProductionMax: Double,
+        hasConnectionError: Bool,
+        lastUpdated: Date?,
+        isAnyCarCharing: Bool,
+        chargingStations: [ChargingStation]
+    ) {
         self.currentSolarProduction = currentSolarProduction
         self.currentOverallConsumption = currentOverallConsumption
         self.currentBatteryLevel = currentBatteryLevel
@@ -53,33 +54,45 @@ class OverviewData: ObservableObject {
         self.isAnyCarCharing = isAnyCarCharing
         self.chargingStations = chargingStations
     }
-    
+
     func isFlowBatteryToHome() -> Bool {
         return currentBatteryChargeRate ?? 0 <= -100
     }
-    
+
     func isFlowSolarToBattery() -> Bool {
         return currentBatteryChargeRate ?? 0 >= 100
     }
-    
+
     func isFlowSolarToHouse() -> Bool {
         return currentSolarToHouse >= 100
     }
-    
+
     func isFlowSolarToGrid() -> Bool {
         return currentSolarToGrid >= minGridIngestionTreashold
     }
-    
+
     func isFlowGridToHouse() -> Bool {
         return currentGridToHouse >= minGridConsumptionTreashold
     }
 }
 
-struct ChargingStation: Codable, Observable {
-    var id: String
-    var name: String
-    var chargingMode: ChargingMode
-    var priority: Int // lower number is higher Priority (ordering)
-    var currentPower: Int // Watt
-    var signal: SensorConnectionStatus?
+class ChargingStation: Observable {
+    @Published var id: String
+    @Published var name: String
+    @Published var chargingMode: ChargingMode
+    @Published var priority: Int  // lower number is higher Priority (ordering)
+    @Published var currentPower: Int  // Watt
+    @Published var signal: SensorConnectionStatus?
+
+    init(
+        id: String, name: String, chargingMode: ChargingMode, priority: Int,
+        currentPower: Int, signal: SensorConnectionStatus? = nil
+    ) {
+        self.id = id
+        self.name = name
+        self.chargingMode = chargingMode
+        self.priority = priority
+        self.currentPower = currentPower
+        self.signal = signal
+    }
 }
