2355ba025ba138c02036a2879eb03f0c616b9a17
Marc Dürst <marc@marcduerst.com>
2024-12-04 19:48:51 +0100

    Fix widget refresh

--- Solar Lens Widgets/Services/SolarLensWidgetDataSource.swift
+++ Solar Lens Widgets/Services/SolarLensWidgetDataSource.swift
@@ -7,11 +7,13 @@ class SolarLensWidgetDataSource {
 
     func getOverviewData() async throws -> OverviewData? {
         
-        if overviewData == nil || lastFetchOverviewData?.timeIntervalSinceNow ?? 0 < -10*60 {
+        if overviewData == nil || lastFetchOverviewData?.timeIntervalSinceNow ?? 0 < -4*60 {
             let data = try? await solarManager.fetchOverviewData(
                 lastOverviewData: overviewData)
 
             overviewData = data;
+            lastFetchOverviewData = Date()
+            print("\(Date().formatted(.iso8601)) - Fetched overview data for widget")
         }
         
         return overviewData;
