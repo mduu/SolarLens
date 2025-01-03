24b94a18ddf4cb0d4c37f881be9cba598c86f331
Marc Dürst <marc@marcduerst.com>
2025-01-03 22:27:27 +0100

    Home screen draft

---
+++ Solar Lens iOS/Components/CircularInstrument.swift
@@ -0,0 +1,43 @@
+//
+//  CircularInstrument.swift
+//  Solar Lens
+//
+//  Created by Marc Dürst on 03.01.2025.
+//
+
+import SwiftUI
+
+struct CircularInstrument: View {
+    var borderColor: Color
+    var label: LocalizedStringResource
+    var value: String
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(.white.opacity(0.8))
+                .overlay(
+                    Circle()
+                        .stroke(borderColor, lineWidth: 3)
+                )
+
+            VStack(alignment: .center) {
+                Text(label)
+                    .font(.system(size: 22, weight: .light))
+                    .multilineTextAlignment(.center)
+                
+                Text(value)
+                    .font(.system(size: 30, weight: .bold))
+            }
+        }
+        .frame(maxWidth: 150, maxHeight: 150)
+    }
+}
+
+#Preview {
+    CircularInstrument(
+        borderColor: .teal,
+        label: "Solar Productions",
+        value: "2.4 kW"
+    )
+}
