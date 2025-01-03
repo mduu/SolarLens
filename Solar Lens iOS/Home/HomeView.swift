da2dfd5aec18a6caf7eecd13a099a745fdbc479e
Marc Dürst <marc@marcduerst.com>
2025-01-03 22:27:27 +0100

    Home screen draft

--- Solar Lens iOS/Home/HomeView.swift
+++ Solar Lens iOS/Home/HomeView.swift
@@ -13,24 +13,107 @@ struct HomeView: View {
 
     var body: some View {
         VStack {
-            if viewModel.isLoading && viewModel.overviewData.lastSuccessServerFetch == nil {
+            if viewModel.isLoading
+                && viewModel.overviewData.lastSuccessServerFetch == nil
+            {
                 ProgressView()
                     .tint(.accent)
                     .padding()
                     .foregroundStyle(.accent)
                     .background(Color.black.opacity(0.3))
             } else {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!")
+                ZStack {
 
-                Text(
-                    "Current Consumption: \(viewModel.overviewData.currentOverallConsumption)"
-                )
+                    Image("OverviewFull")
+                        .resizable()
+                        .scaledToFill()
+                        .saturation(0.6)
+                        .ignoresSafeArea()
+
+                    VStack {
+                        Spacer()
+
+                        HStack(alignment: .center) {
+                            Image("solarlens")
+                                .resizable()
+                                .scaledToFit()
+                                .cornerRadius(5)
+                                .frame(maxWidth: 50)
+
+                            VStack(alignment: .leading) {
+
+                                Text("Solar")
+                                    .foregroundColor(.accent)
+                                    .font(.system(size: 24, weight: .bold))
+
+                                Text("Lens")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 24, weight: .bold))
+
+                            }
+
+                        }  // :HStack
+
+                    }  // :VStack
+
+                    VStack {
+
+                        var solar =
+                            Double(
+                                viewModel.overviewData.currentSolarProduction)
+                            / 1000
+                        
+                        var consumption =
+                            Double(
+                                viewModel.overviewData.currentOverallConsumption)
+                            / 1000
+                        
+                        var grid =
+                            Double(
+                                viewModel.overviewData.currentGridToHouse >= 0
+                                ? viewModel.overviewData.currentGridToHouse
+                                : viewModel.overviewData.currentSolarToGrid)
+                            / 1000
+                        
+                        var battery = viewModel.overviewData.currentBatteryLevel ?? 0
+                        
+                        HStack(spacing: 50) {
+                            CircularInstrument(
+                                borderColor: Color.accentColor,
+                                label: "Solar Production",
+                                value: String(format: "%.1f kW", solar)
+                            )
+                            
+                            CircularInstrument(
+                                borderColor: Color.orange,
+                                label: "Grid",
+                                value: String(format: "%.1f kW", grid)
+                            )
+                        }
+                        .padding(.top, 50)
+                        
+                        HStack(spacing: 50) {
+                            CircularInstrument(
+                                borderColor: Color.green,
+                                label: "Battery",
+                                value: String(format: "%.0f %%", battery)
+                            )
+                            
+                            CircularInstrument(
+                                borderColor: Color.teal,
+                                label: "Consumption",
+                                value: String(format: "%.1f kW", consumption)
+                            )
+                        }
+                        .padding(.top, 100)
+                        
+                        Spacer()
+
+                    }  // :VStack
+                    .padding(.top, 50)
+                }  // :ZStack
             }
         }
-        .padding()
         .onAppear {
             if viewModel.overviewData.lastSuccessServerFetch == nil {
                 print("fetch on appear")
