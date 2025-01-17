ef64b30939956fb9a060ff6aecd44bc55d9bdbc1
Marc Dürst <marc@marcduerst.com>
2025-01-17 21:47:33 +0100

    Fixes

--- Shared/AppIntents/GetBatteryLevelIntent.swift
+++ Shared/AppIntents/GetBatteryLevelIntent.swift
@@ -2,15 +2,16 @@ import AppIntents
 
 struct GetBatteryLevelIntent : AppIntent {
     static var title: LocalizedStringResource = "Get current battery level"
+    static var description: IntentDescription? = "Get the current battery level in percent"
 
     @MainActor
-    func perform() async throws -> some IntentResult & ReturnsValue<Double> {
+    func perform() async throws -> some IntentResult & ReturnsValue<Int> {
         let enegeryManager = SolarManager()
         let solarProduction = try? await enegeryManager.fetchOverviewData(lastOverviewData: nil)
         
         if let batteryLevel = solarProduction?.currentBatteryLevel {
             return .result(
-                value: Double(batteryLevel / 1000)
+                value: batteryLevel
             )
         }
         
