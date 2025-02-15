aa7bebb58ce05181335a97afcbf88046454cd3e0
Marc Dürst <marc@marcduerst.com>
2025-02-15 20:04:14 +0100

    Fix font-size for Forecast values

--- Shared/Views/Solar/ForecastItemView.swift
+++ Shared/Views/Solar/ForecastItemView.swift
@@ -89,7 +89,7 @@ struct ForecastItemView: View {
                         #if os(watchOS)
                             .font(.headline)
                         #else
-                            .font(.subheadline)
+                            .font(.caption)
                         #endif
 
                     if !isSmall {
@@ -138,7 +138,8 @@ struct ForecastItemView: View {
             ForecastItem(min: 0.2, max: 0.4, expected: 0.3),
             ForecastItem(min: 3.2, max: 3.4, expected: 3.3),
         ],
-        forecast: ForecastItem(min: 1.2, max: 1.4, expected: 1.3),
+        forecast: ForecastItem(min: 11, max: 30.4, expected: 1.3),
         small: true
     )
+    .frame(maxWidth: 30, maxHeight: 60)
 }
