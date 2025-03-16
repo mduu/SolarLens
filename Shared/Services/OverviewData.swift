7d88dbf116c1887fe053c645d37a31172a207b7d
Marc Dürst <marc@marcduerst.com>
2025-03-16 11:49:22 +0100

    Revert

--- Shared/Services/OverviewData.swift
+++ Shared/Services/OverviewData.swift
@@ -280,7 +280,7 @@ class Device: Identifiable {
     }
     
     func hasPower() -> Bool {
-        return currentPowerInWatts > 9 || currentPowerInWatts < -9
+        return currentPowerInWatts > 10 || currentPowerInWatts < -10
     }
     
     func isConsumingDevice() -> Bool {
