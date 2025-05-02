f2dd38939e371654d8839d63316061068f99cc52
Marc Dürst <marc@marcduerst.com>
2025-05-02 07:47:41 +0200

    Add Siri tips to settings menu

--- Solar Lens iOS/Settings/SiriInfoView.swift
+++ Solar Lens iOS/Settings/SiriInfoView.swift
@@ -1,10 +1,37 @@
-// 
-
 import SwiftUI
+import _AppIntents_SwiftUI
 
 struct SiriInfoView: View {
+    fileprivate func SiriTip(for intent: any AppIntent) -> some View {
+        return SiriTipView(
+            intent: intent,
+            isVisible: .constant(true)
+        )
+        .siriTipViewStyle(.automatic)
+        .padding(.horizontal)
+        .padding(.top)
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(
+                "You can use Siri to query and control your Solar Manager using Solar Lens. This works everywhere where Siri works and therefore also in CarPlay etc. \n\nHere are some examples:"
+            ).padding()
+
+            ScrollView {
+
+                SiriTip(for: GetSolarProductionIntent())
+                SiriTip(for: GetBatteryLevelIntent())
+                SiriTip(for: GetConsumptionIntent())
+                SiriTip(for: IsAnyCarChargingIntent())
+                SiriTip(for: GetForecastIntent())
+                SiriTip(for: GetCarInfosIntent())
+                SiriTip(for: SetChargingModeIntent())
+
+            }
+
+            Spacer()
+        }.navigationTitle("Discover Siri")
     }
 }
 
