66d51051e56dd468b7afd72dac686f6ba4443292
Marc Dürst <marc@marcduerst.com>
2025-08-12 21:01:50 +0200

    tvOS Target and AppIcons

---
+++ Solar Lens BigScreen/Solar_Lens_BigScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Solar_Lens_BigScreenApp.swift
+//  Solar Lens BigScreen
+//
+//  Created by Marc Dürst on 12.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct Solar_Lens_BigScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
