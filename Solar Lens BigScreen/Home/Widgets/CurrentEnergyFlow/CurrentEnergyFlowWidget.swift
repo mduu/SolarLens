be0aa45b2f3084cf1c72ca0deb6c29c7c0f22a6e
Marc Dürst <marc@marcduerst.com>
2025-10-05 09:06:26 +0200

    Improve CurrentFlowWidget

--- Solar Lens BigScreen/Home/Widgets/CurrentEnergyFlow/CurrentEnergyFlowWidget.swift
+++ Solar Lens BigScreen/Home/Widgets/CurrentEnergyFlow/CurrentEnergyFlowWidget.swift
@@ -4,74 +4,82 @@ struct CurrentEnergyFlowWidget: View {
     @Environment(CurrentBuildingState.self) var buildings: CurrentBuildingState
 
     var body: some View {
-        Grid {
-            GridRow {
-                SolarView(
-                    currentSolarProductionInW: buildings.overviewData
-                        .currentSolarProduction
-                )
-                .frame(maxWidth: .infinity)
+        VStack {
+            HStack {
+                Text("Now")
+                    .font(.title2)
+            }
 
-                ArrowSolarToGrid(
-                    isActive: buildings.overviewData.isFlowSolarToGrid()
-                )
-                .frame(maxWidth: .infinity)
+            Grid {
+                GridRow {
+                    SolarView(
+                        currentSolarProductionInW: buildings.overviewData
+                            .currentSolarProduction
+                    )
+                    .frame(maxWidth: .infinity)
 
-                CurrentGridView(
-                    currentGridInW:
-                        buildings.overviewData.isFlowSolarToGrid()
+                    ArrowSolarToGrid(
+                        isActive: buildings.overviewData.isFlowSolarToGrid()
+                    )
+                    .frame(maxWidth: .infinity)
+
+                    CurrentGridView(
+                        currentGridInW:
+                            buildings.overviewData.isFlowSolarToGrid()
                         ? buildings.overviewData.currentSolarToGrid
                         : buildings.overviewData.currentGridToHouse
-                )
-                .frame(maxWidth: .infinity)
+                    )
+                    .frame(maxWidth: .infinity)
+                }
+
+                GridRow {
+
+                    ArrowSolarToBattery(
+                        isActive: buildings.overviewData.isFlowSolarToBattery()
+                    )
+                    .frame(maxWidth: .infinity)
+
+                    ArrowSolarToHouse(
+                        isActive: buildings.overviewData.isFlowSolarToHouse()
+                    )
+                    .frame(maxWidth: .infinity)
+
+                    ArrowGridToHouse(
+                        isActive: buildings.overviewData.isFlowGridToHouse()
+                    )
+                    .frame(maxWidth: .infinity)
+
+                }
+
+                GridRow {
+
+                    CurrentBatteryView(
+                        currentBatteryLevel: buildings.overviewData
+                            .currentBatteryLevel,
+                        currentChargeRate: buildings.overviewData
+                            .currentBatteryChargeRate
+                    )
+                    .frame(maxWidth: .infinity)
+
+                    ArrowBatteryToHouse(
+                        isActive: buildings.overviewData.isFlowBatteryToHome()
+                    )
+                    .frame(maxWidth: .infinity)
+
+                    ConsumptionView(
+                        currentOverallConsumptionInW: buildings.overviewData
+                            .currentOverallConsumption
+                    )
+                    .frame(maxWidth: .infinity)
+
+                }
             }
-
-            GridRow {
-
-                ArrowSolarToBattery(
-                    isActive: buildings.overviewData.isFlowSolarToBattery()
-                )
-                .frame(maxWidth: .infinity)
-
-                ArrowSolarToHouse(
-                    isActive: buildings.overviewData.isFlowSolarToHouse()
-                )
-                .frame(maxWidth: .infinity)
-
-                ArrowGridToHouse(
-                    isActive: buildings.overviewData.isFlowGridToHouse()
-                )
-                .frame(maxWidth: .infinity)
-
-            }
-
-            GridRow {
-
-                CurrentBatteryView(
-                    currentBatteryLevel: buildings.overviewData
-                        .currentBatteryLevel,
-                    currentChargeRate: buildings.overviewData
-                        .currentBatteryChargeRate
-                )
-                .frame(maxWidth: .infinity)
-
-                ArrowBatteryToHouse(
-                    isActive: buildings.overviewData.isFlowBatteryToHome()
-                )
-                .frame(maxWidth: .infinity)
-
-                ConsumptionView(
-                    currentOverallConsumptionInW: buildings.overviewData
-                        .currentOverallConsumption
-                )
-                .frame(maxWidth: .infinity)
-
-            }
+            .frame(height: 350)
+            .padding(.horizontal, 10)
         }
+        .padding(20)
         .foregroundColor(.white.opacity(0.9))
-        .padding(50)
         .frame(maxWidth: .infinity)
-        .frame(height: 350)
         .glassEffect(.clear, in: .rect(cornerRadius: 30.0))
 
     }
