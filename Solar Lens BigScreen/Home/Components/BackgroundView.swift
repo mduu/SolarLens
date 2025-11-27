7af245a0f1ab5c8a40f9c45404688928605ed3d8
Marc Dürst <marc@marcduerst.com>
2025-11-27 06:24:56 +0100

    tvOS: Add many more backgrounds

--- Solar Lens BigScreen/Home/Components/BackgroundView.swift
+++ Solar Lens BigScreen/Home/Components/BackgroundView.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 struct BackgroundView: View {
-    @AppStorage("backgroundImage") var backgroundImage: String?
+    @AppStorage("backgroundImageV2") var backgroundImage: String?
 
     var body: some View {
         Image(backgroundImage ?? "bg_blue_sunny_clouds_4k")
--- Solar Lens BigScreen/Settings/BackgroundConfiguratonView.swift
+++ Solar Lens BigScreen/Settings/BackgroundConfiguratonView.swift
@@ -1,11 +1,19 @@
 import SwiftUI
 
 struct BackgroundConfiguratonView: View {
-    @AppStorage("backgroundImage") var backgroundImage: String = availableBackgroundImages.first!.key
+    @AppStorage("backgroundImageV2") var backgroundImage: String = availableBackgroundImages.first!.key
 
     static let availableBackgroundImages: [String: LocalizedStringKey] = [
         "bg_blue_sunny_clouds_4k": "Blue Sky",
-        "bg_dark_sunny_clouds.4k": "Dark Sky",
+        "bg_dark_sunny_clouds_4k": "Dark Sky",
+        "bg_green_leaf_4k": "Green Leaf",
+        "bg_neon_solar_4k": "Neon Solar",
+        "bg_orange_gradient_4k": "Orange Gradient",
+        "bg_orange_sky_4k": "Orange Sky",
+        "bg_rainbow_panels_4k": "Rainbow Panels",
+        "bg_solar_roof_4k": "Solar Roof",
+        "bg_solar_sunset_4k": "Solar Sunset",
+        "bg_sunny_forrest_4k": "Sunny Forrest",
     ]
 
     var body: some View {
